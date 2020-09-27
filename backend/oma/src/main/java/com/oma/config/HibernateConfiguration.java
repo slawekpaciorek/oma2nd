240fc79e94ade0e268d639ccd58aeb56576bc32b
slawek <slawekpaciorek@gmail.com>
2020-09-27 02:27:08 +0200

    Add hibernate adnotations to models

--- backend/oma/src/main/java/com/oma/config/HibernateConfiguration.java
+++ backend/oma/src/main/java/com/oma/config/HibernateConfiguration.java
@@ -1,8 +1,13 @@
 package com.oma.config;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.mysql.cj.jdbc.Driver;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -15,23 +20,26 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class HibernateConfiguration {
 
+    @Autowired
+    Environment environment;
+
     @Bean
     public LocalSessionFactoryBean sessionFactoryBean(){
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
         sessionFactoryBean.setDataSource(dataSource());
         sessionFactoryBean.setPackagesToScan("com.oma.model");
-        sessionFactoryBean.setHibernateProperties(properties());
+        sessionFactoryBean.setHibernateProperties(hibernateProps());
         return sessionFactoryBean;
     }
 
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .driverClassName("spring.datasource.driver-class-name")
-                .url("spring.datasource.url")
-                .username("spring.datasource.username")
-                .password("spring.datasource.password")
-                .build();
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+        return dataSource;
     }
 
     @Bean
@@ -42,12 +50,10 @@ public class HibernateConfiguration {
     }
 
 
-    private Properties properties(){
+    private Properties hibernateProps(){
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.dialect.H2Dialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
     }
-
-
 }
--- backend/oma/src/main/java/com/oma/model/Address.java
+++ backend/oma/src/main/java/com/oma/model/Address.java
@@ -2,14 +2,33 @@ package com.oma.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
+//@RequiredArgsConstructor
+@Entity
 public class Address {
 
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
     private String streetNameAndNumber;
+
+    @Column
     private String zipCode;
+
+    @Column
     private String City;
+
+    @Column
     private int mobilePhoneNumber;
 
 }
