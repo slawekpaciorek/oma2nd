2ae1dc3598bf89af816bb36af755a631a6a67506
glk <52168046+mgrzelczyk@users.noreply.github.com>
2020-09-29 18:56:55 +0200

    Add logback dependency (http://logback.qos.ch/index.html) for slf4j logging, add simple trace log for running application

--- backend/oma/src/main/java/com/oma/OrderManagementApplication.java
+++ backend/oma/src/main/java/com/oma/OrderManagementApplication.java
@@ -1,12 +1,17 @@
 package com.oma;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderManagementApplication {
 
+	private static Logger log = LoggerFactory.getLogger(OrderManagementApplication.class);
+
 	public static void main(String[] args) {
+		log.trace("Application is running");
 		SpringApplication.run(OrderManagementApplication.class, args);
 	}
 
