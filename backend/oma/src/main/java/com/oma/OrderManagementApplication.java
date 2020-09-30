4f4a52eaa19d09ec37c816db0ef1b6301c7b6b1b
glk <52168046+mgrzelczyk@users.noreply.github.com>
2020-09-30 18:35:32 +0200

    Return to logback, add configuration and in propertiec config of file name

--- backend/oma/src/main/java/com/oma/OrderManagementApplication.java
+++ backend/oma/src/main/java/com/oma/OrderManagementApplication.java
@@ -1,7 +1,7 @@
 package com.oma;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
