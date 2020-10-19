d4cb0090353352be91704129250bf488d46ffb16
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-19 18:42:20 +0200

    Add DAO and Service Company in project. First version.

---
+++ backend/oma/src/test/java/com/oma/dao/CompanyDAOTest.java
@@ -0,0 +1,32 @@
+package com.oma.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyDAOTest {
+
+    @Test
+    void getAllCompany() {
+    }
+
+    @Test
+    void saveCompany() {
+    }
+
+    @Test
+    void getCompanyForID() {
+    }
+
+    @Test
+    void getCompanyForTaxNumberId() {
+    }
+
+    @Test
+    void updateCompany() {
+    }
+
+    @Test
+    void removeCompany() {
+    }
+}
