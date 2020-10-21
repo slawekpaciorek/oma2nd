092afbbc3eb329af1ef90a733ca4a07641d4ee48
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-21 21:32:35 +0200

    Generated getter & setter and equals & hashCode in model Company.java & User.java

--- backend/oma/src/main/java/com/oma/model/Company.java
+++ backend/oma/src/main/java/com/oma/model/Company.java
@@ -2,9 +2,11 @@ package com.oma.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -35,8 +37,75 @@ public class Company {
     @OneToMany(mappedBy = "company")
     private List<DeliveryPoint> deliveryPoints;
 
-    public Company(String name) {
+//Dodane gettery i settery
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+    public int getTaxNumberId() {
+        return taxNumberId;
+    }
+
+    public void setTaxNumberId(int taxNumberId) {
+        this.taxNumberId = taxNumberId;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress() {
+        this.address = address;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<ProductsOrder> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<ProductsOrder> orders) {
+        this.orders = orders;
+    }
+
+    public List<DeliveryPoint> getDeliveryPoints() {
+        return deliveryPoints;
+    }
+
+    public void setDeliveryPoints(List<DeliveryPoint> deliveryPoints) {
+        this.deliveryPoints = deliveryPoints;
+    }
+    //Wygenerowany equals i hashCode 20.10.2020.. środa
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return id == company.id &&
+                name.equals(company.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
