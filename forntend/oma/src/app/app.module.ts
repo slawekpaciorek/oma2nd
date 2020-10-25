fd1f99d4d4985d68ed6beed016cd1368dd6049d5
slawek <slawekpaciorek@gmail.com>
2020-10-25 10:37:29 +0100

    Basic View

--- forntend/oma/src/app/app.module.ts
+++ forntend/oma/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+import { RouterModule } from '@angular/router';
+import { MainViewComponent } from './main-view/main-view.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserViewComponent,
+    AdminViewComponent,
+    MainViewComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'user-view', component: UserViewComponent},
+      {path: 'admin-view', component: AdminViewComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
