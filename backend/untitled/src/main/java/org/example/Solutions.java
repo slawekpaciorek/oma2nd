f599c9f94656dac589192f0701107de0a76f7e12
slawek <slawekpaciorek@gmail.com>
2020-10-27 19:49:29 +0100

    removed untitled folder

--- backend/untitled/src/main/java/org/example/Solutions.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Solutions {
-    public boolean isEgoistic(String N) {
-        String lastItem = N.substring(N.length()-1);
-        return lastItem.equals(String.valueOf((int)Math.pow(Double.parseDouble(lastItem),2)).substring(1));
-
-    }
-}
