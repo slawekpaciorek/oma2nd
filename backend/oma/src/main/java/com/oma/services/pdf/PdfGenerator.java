4d10578e710e2e0ed9d61aa380411fe939147338
glk <52168046+mgrzelczyk@users.noreply.github.com>
2021-01-08 20:00:50 +0100

    Added logger in PdfGenerator class

--- backend/oma/src/main/java/com/oma/services/pdf/PdfGenerator.java
+++ backend/oma/src/main/java/com/oma/services/pdf/PdfGenerator.java
@@ -1,6 +1,12 @@
 package com.oma.services.pdf;
 
+import com.oma.OrderManagementApplication;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 @Service
 public class PdfGenerator {
+
+    private static Logger log = LoggerFactory.getLogger(OrderManagementApplication.class);
 
 }
