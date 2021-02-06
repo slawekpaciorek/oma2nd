becce563e9afd46c09b8cb66fce65ecd481b71da
Slawek Paciorek <slawekpaciorek@gmail.com>
2021-02-06 21:07:02 +0100

    delivery point controller

---
+++ backend/oma/src/main/java/com/oma/controllers/DeliveryPointController.java
@@ -0,0 +1,14 @@
+package com.oma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("delivery-points")
+@CrossOrigin({"http://localhost:4200", "http://localhost:5555"})
+public class DeliveryPointController {
+
+
+}
