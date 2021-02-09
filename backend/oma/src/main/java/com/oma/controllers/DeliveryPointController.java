9cf4858b03b2799e41cc1fdb7748f00563dfd679
Slawek Paciorek <slawekpaciorek@gmail.com>
2021-02-09 22:14:25 +0100

    add methods to deliveryPoint controller

--- backend/oma/src/main/java/com/oma/controllers/DeliveryPointController.java
+++ backend/oma/src/main/java/com/oma/controllers/DeliveryPointController.java
@@ -1,10 +1,11 @@
 package com.oma.controllers;
 
+import com.oma.model.DeliveryPoint;
 import com.oma.services.DeliveryPointService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("delivery-points")
@@ -13,5 +14,25 @@ public class DeliveryPointController {
 
     @Autowired
     DeliveryPointService deliveryPointService;
+
+    @GetMapping("/list")
+    public List<DeliveryPoint> displayAllDeliveryPoints(){
+        return null;
+    }
+
+    @PostMapping(value = "/add", consumes = "application/json")
+    public void addDeliveryPoint(@RequestBody DeliveryPoint deliveryPoint){
+
+    }
+
+    @PutMapping(value = "/update", consumes = "application/json")
+    public void updateDeliveryPoint(@RequestParam String id, @RequestBody DeliveryPoint deliveryPoint){
+
+    }
+
+    @DeleteMapping(value = "/remove")
+    public void removeDeliveryPoint(@RequestParam String id){
+        
+    }
 
 }
