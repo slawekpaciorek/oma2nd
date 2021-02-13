b3c07d5e282ea2743a8af6f899b0df7ed66d7bc7
Slawek Paciorek <slawekpaciorek@gmail.com>
2021-02-13 18:32:43 +0100

    Add interface and implementation for order DAO

---
+++ backend/oma/src/main/java/com/oma/dao/OrderDAOImplementation.java
@@ -0,0 +1,69 @@
+package com.oma.dao;
+
+import com.oma.model.ProductsOrder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class OrderDAOImplementation implements OrderDao {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    private Logger log = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Transactional
+    @Override
+    public ProductsOrder getOrderById(long id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from ProductsOrder o where o.id = :id", ProductsOrder.class).setParameter("id", id).getSingleResult();
+    }
+
+    @Transactional
+    @Override
+    public List<ProductsOrder> getOrderList() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from ProductsOrder", ProductsOrder.class).getResultList();
+    }
+
+    @Transactional
+    @Override
+    public void saveOrder(ProductsOrder order) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(order);
+    }
+
+    @Transactional
+    @Override
+    public void updateOrder(long id, ProductsOrder order) {
+        ProductsOrder orderToUpdate = getOrderById(id);
+        if(!orderToUpdate.equals(order)){
+            if(!orderToUpdate.getDeliveryPoint().equals(order.getDeliveryPoint()))
+                orderToUpdate.setDeliveryPoint(order.getDeliveryPoint());
+            if(!orderToUpdate.getStatus().equals(order.getStatus()))
+                orderToUpdate.setStatus(order.getStatus());
+            if(!orderToUpdate.getInfo().equals(order.getInfo()))
+                orderToUpdate.setInfo(order.getInfo());
+            Session session = sessionFactory.getCurrentSession();
+            session.update(orderToUpdate);
+        }
+        else{
+            log.info("Objects are the same");
+        }
+    }
+
+    @Transactional
+    @Override
+    public void removeOrder(long id){
+        ProductsOrder order = getOrderById(id);
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(order);
+    }
+}
---
+++ backend/oma/src/test/java/com/oma/dao/OrderDAOImplementationTest.java
@@ -0,0 +1,84 @@
+package com.oma.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderDAOImplementationTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void shouldGetOrderById() {
+
+        //  given
+
+
+        //  when
+
+
+        //  then
+
+
+    }
+
+    @Test
+    void shouldGetOrderList() {
+
+        //  given
+
+
+        //  when
+
+
+        //  then
+
+
+    }
+
+    @Test
+    void shouldSaveOrder() {
+
+        //  given
+
+
+        //  when
+
+
+        //  then
+
+
+    }
+
+    @Test
+    void shouldUpdateOrder() {
+
+        //  given
+
+
+        //  when
+
+
+        //  then
+
+
+    }
+
+    @Test
+    void shouldRemoveOrder() {
+
+        //  given
+
+
+        //  when
+
+
+        //  then
+
+
+    }
+    
+}
