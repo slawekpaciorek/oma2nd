ca251f3b3b38bb9401260ef36626f904e1465cfd
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-16 21:08:23 +0100

    Add workspace on test method

---
+++ backend/oma/src/test/java/com/oma/controllers/ParserCsvProductControllerTest.java
@@ -0,0 +1,12 @@
+package com.oma.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserCsvProductControllerTest {
+
+    @Test
+    void findProducts() {
+    }
+}
---
+++ backend/oma/src/test/java/com/oma/prasercsv/WriteCsvProductResponseTest.java
@@ -0,0 +1,26 @@
+package com.oma.prasercsv;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WriteCsvProductResponseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void writeProducts() {
+    }
+
+    @Test
+    void writeProduct() {
+    }
+}
