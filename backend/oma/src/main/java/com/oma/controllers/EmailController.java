53b49d5fe0f5438b1787c177ae93bc08c415bcd2
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-16 19:05:42 +0100

    Injection EmailSenderService #49

--- backend/oma/src/main/java/com/oma/controllers/EmailController.java
+++ backend/oma/src/main/java/com/oma/controllers/EmailController.java
@@ -1,7 +1,9 @@
 package com.oma.controllers;
 
+import com.oma.utils.EmailSenderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailController.class);
+
+    @Autowired
+    EmailSenderService emailSenderService;
 }
