037ae6627f20c91319abe220a0d5bd30f21d48e6
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-16 19:04:06 +0100

    Add logger to EmailController.class #49

---
+++ backend/oma/src/main/java/com/oma/controllers/EmailController.java
@@ -0,0 +1,15 @@
+package com.oma.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+@CrossOrigin({"http://localhost:4200", "http://localhost:5555"})
+public class EmailController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailController.class);
+}
