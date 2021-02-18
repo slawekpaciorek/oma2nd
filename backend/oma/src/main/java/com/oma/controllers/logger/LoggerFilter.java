d75505804b32b3d8b29f813759479d5a8f954ebc
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-18 21:33:18 +0100

    Implements Filter to LoggerFilter #62

--- backend/oma/src/main/java/com/oma/controllers/logger/LoggerFilter.java
+++ backend/oma/src/main/java/com/oma/controllers/logger/LoggerFilter.java
@@ -1,4 +1,30 @@
 package com.oma.controllers.logger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
 public class LoggerFilter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            var httpRequest = (HttpServletRequest) request;
+            logger.info(" [doFilter] " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+        }
+        chain.doFilter(request, response);
+        logger.info(" [doFilter] 2 ");
+
+    }
 }
