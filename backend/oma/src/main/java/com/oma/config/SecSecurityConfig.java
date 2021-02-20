79f5abad0ca0a6e444e3e984045c9eb77095d26a
Marcin_Grzelczyk <m.grzelczyk@imgw.pl>
2021-02-20 20:45:43 +0100

    Class SecSecurityConfig

---
+++ backend/oma/src/main/java/com/oma/config/SecSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.oma.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+     }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+     }
+}
