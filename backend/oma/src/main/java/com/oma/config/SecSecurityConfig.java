faf85c986d917a1eed3771bfe6a7412b68eb3e15
Marcin_Grzelczyk <m.grzelczyk@imgw.pl>
2021-02-20 20:56:50 +0100

    Class AppInitializer

--- backend/oma/src/main/java/com/oma/config/SecSecurityConfig.java
+++ backend/oma/src/main/java/com/oma/config/SecSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.oma.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,9 +15,39 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-     }
+        auth.inMemoryAuthentication()
+            .withUser("user").password(passwordEncoder().encode("userPass")).roles("USER")
+            .and()
+            .withUser("manager").password(passwordEncoder().encode("managerPass")).roles("USER")
+            .and()
+            .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+    }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/anonymous*").anonymous()
+            .antMatchers("/login*").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login.html")
+            .loginProcessingUrl("/perform_login")
+            .defaultSuccessUrl("/homepage.html", true)
+            .failureUrl("/login.html?error=true")
+            .failureHandler(authenticationFailureHandler())
+            .and()
+            .logout()
+            .logoutUrl("/perform_logout")
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler(logoutSuccessHandler());
      }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
