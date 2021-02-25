b3be185085dcfb22c1301c990a74ed1e99d0d628
Marcin_Grzelczyk <m.grzelczyk@imgw.pl>
2021-02-25 17:32:06 +0100

    Add siple log to trace login in

--- backend/oma/src/main/java/com/oma/controllers/WebController.java
+++ backend/oma/src/main/java/com/oma/controllers/WebController.java
@@ -1,6 +1,8 @@
 package com.oma.controllers;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WebController {
 
+    private static Logger log = LoggerFactory.getLogger(WebController.class);
+
     @RequestMapping("/")
     @ResponseBody
     public String index() {
+        log.trace("User is logging successfully)");
         return "Hello World! Welcome";
     }
 
