8b91bc71609b19704992676ce24d150b0ca7727b
Marcin_Grzelczyk <m.grzelczyk@imgw.pl>
2021-02-25 19:25:04 +0100

    Security test

--- backend/oma/src/test/java/com/oma/controllers/LoginControllerTest.java
+++ backend/oma/src/test/java/com/oma/controllers/LoginControllerTest.java
@@ -1,12 +1,23 @@
 package com.oma.controllers;
 
+import com.oma.config.SecurityConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginControllerTest {
 
+    @Autowired
+    private SecurityConfig service;
+
+    @Test(expected = AuthenticationCredentialsNotFoundException.class)
+    public void givenUnauthenticated_whenCallService_thenThrowsException() {
+        service.passwordEncoder();
+    }
 
 }
