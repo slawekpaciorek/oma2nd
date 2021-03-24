06bbb81f9049f9e91c3ad839318fb3a602582f04
Marcin_Grzelczyk <m.grzelczyk@imgw.pl>
2021-03-24 16:31:15 +0100

    Update

--- forntend/oma/src/app/app-routing.module.ts
+++ forntend/oma/src/app/app-routing.module.ts
@@ -1,36 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from './user/user.component';
+import {PmComponent} from './pm/pm.component';
+import {HomeComponent} from './home/home.component';
+import * as path from 'path';
+import {AdminComponent} from './admin/admin.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   path: 'home',
-  component: HomeComponent
+  component: HomeComponent;
 },
 {
   path: 'user',
-    component: UserComponent
+    component;: UserComponent;
 },
 {
   path: 'pm',
-    component: PmComponent
+    component;: PmComponent;
 },
 {
   path: 'admin',
-    component: AdminComponent
+    component;: AdminComponent;
 },
 {
   path: 'auth/login',
-    component: LoginComponent
+    component;: LoginComponent;
 },
 {
   path: 'signup',
-    component: RegisterComponent
+    component;: RegisterComponent;
 },
 {
   path: '',
-    redirectTo: 'home',
-  pathMatch: 'full'
+    redirectTo;: 'home',
+  pathMatch;: 'full';
 }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
