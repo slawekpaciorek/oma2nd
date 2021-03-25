8bd30f33b0705e42e1149b9a73f9b1c3aaf38426
glk <52168046+mgrzelczyk@users.noreply.github.com>
2021-03-25 18:03:18 +0100

    Update and error fix app-routing.module.ts

--- forntend/oma/src/app/app-routing.module.ts
+++ forntend/oma/src/app/app-routing.module.ts
@@ -3,41 +3,41 @@ import { Routes, RouterModule } from '@angular/router';
 import {UserComponent} from './user/user.component';
 import {PmComponent} from './pm/pm.component';
 import {HomeComponent} from './home/home.component';
-import * as path from 'path';
 import {AdminComponent} from './admin/admin.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
-  path: 'home',
-  component: HomeComponent;
-},
-{
-  path: 'user',
-    component;: UserComponent;
-},
-{
-  path: 'pm',
-    component;: PmComponent;
-},
-{
-  path: 'admin',
-    component;: AdminComponent;
-},
-{
-  path: 'auth/login',
-    component;: LoginComponent;
-},
-{
-  path: 'signup',
-    component;: RegisterComponent;
-},
-{
-  path: '',
-    redirectTo;: 'home',
-  pathMatch;: 'full';
-}
-]
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'pm',
+    component: PmComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
