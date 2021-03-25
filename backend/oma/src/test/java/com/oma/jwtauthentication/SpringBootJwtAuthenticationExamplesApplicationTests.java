abfc9ce1b2d21026f742afb7b4211b005d4c8c62
glk <52168046+mgrzelczyk@users.noreply.github.com>
2021-03-25 17:45:01 +0100

    Add jwtauthentication simple test

---
+++ backend/oma/src/test/java/com/oma/jwtauthentication/SpringBootJwtAuthenticationExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oma.jwtauthentication
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
